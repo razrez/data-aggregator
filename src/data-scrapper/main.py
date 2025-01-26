7d8a4321ebc8b6107b12ec8390ec7d65e4b11854
ramazanix <ramazanui2002@gmail.com>
2025-01-27 01:37:54 +0300

    Implemented posts data collection

--- src/data-scrapper/main.py
+++ src/data-scrapper/main.py
@@ -5,6 +5,8 @@ import requests
 load_dotenv()
 
 access_token = getenv("VK_ACCESS_TOKEN")
+vk_api_version = getenv("VK_API_VERSION")
+posts_count = getenv("POSTS_COUNT")
 
 public_domains = [
     "vk",
@@ -18,14 +20,31 @@ public_domains = [
 ]
 
 urls_gen = (
-    f"https://api.vk.com/method/wall.get?domain={domain}&access_token={access_token}&v=5.199&count=1"
+    f"https://api.vk.com/method/wall.get?domain={domain}&access_token={access_token}&v={vk_api_version}&count={posts_count}"
     for domain in public_domains
 )
 
 
 def make_request(url):
     response = requests.get(url)
+    print("response", response.status_code, url)
     return response.json()
 
 
-print(make_request(next(urls_gen)))
+def extract_data(data: dict):
+    posts = data["response"]["items"]
+    for post in posts:
+        post_data = {}
+        post_data["id"] = post["id"]
+        post_data["date"] = post["date"]
+        post_data["text"] = post["text"]
+        post_data["likes"] = post["likes"]["count"]
+        post_data["views"] = post["views"]["count"]
+        yield post_data
+
+
+if __name__ == "__main__":
+    for url in urls_gen:
+        data = make_request(url)
+        for post_data in extract_data(data):
+            print(post_data)
