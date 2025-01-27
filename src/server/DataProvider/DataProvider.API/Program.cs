de32a2fcdccfb74de4a170f8462042d3bebcc682
s.khasanov <s.khasanov>
2025-01-27 17:58:25 +0300

    feat: added getting a list of posts

--- src/server/DataProvider/DataProvider.API/Program.cs
+++ src/server/DataProvider/DataProvider.API/Program.cs
@@ -1,35 +1,50 @@
-using System.Text.Json.Serialization;
+var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateSlimBuilder(args);
-
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-  options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
+builder.Services.Configure<InfinispanSettings>(builder.Configuration.GetSection("InfinispanSettings"));
 
 var app = builder.Build();
 
-var sampleTodos = new Todo[] {
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-};
+// Получение настроек Infinispan
+var infinispanSettings = app.Services.GetRequiredService<IConfiguration>().GetSection("InfinispanSettings").Get<InfinispanSettings>();
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
+HttpClient httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{infinispanSettings.User}:{infinispanSettings.Password}")));
+
+app.MapGet("/get/{key}", async (string key) =>
+{
+    var response = await httpClient.GetAsync($"{infinispanSettings.Url}/rest/v2/caches/cache-name/{key}");
+    if (response.IsSuccessStatusCode)
+    {
+        var value = await response.Content.ReadAsStringAsync();
+        return Results.Ok(new { Key = key, Value = value });
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
+
+app.MapGet("/list", async (int offset = 0, int limit = 10) =>
+{
+    var response =
+        await httpClient.GetAsync($"{infinispanSettings.Url}/rest/v2/caches/cache-name?max={limit}&start={offset}");
+    if (response.IsSuccessStatusCode)
+    {
+        var values = await response.Content.ReadFromJsonAsync<List<string>>();
+        return Results.Ok(values);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
 
 app.Run();
 
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
-[JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
+// Модель для настроек Infinispan
+public class InfinispanSettings
 {
-
+    public string Url { get; set; }
+    public string User { get; set; }
+    public string Password { get; set; }
 }
