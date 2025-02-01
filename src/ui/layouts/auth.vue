f5c1506bf05c3882546268bca8db480f0da4d416
s.khasanov <s.khasanov>
2025-02-01 14:00:13 +0300

    feat: fixed auth pages

---
+++ src/ui/layouts/auth.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <el-row>
+          <el-col :span="12">
+            <NuxtLink class="link" to="/">
+              <div class="logo">
+                <el-image :src="'/vk-logo.svg'" fit="fill" style="width: 30px; height: 30px"></el-image>
+                VK Analyzer
+              </div>
+            </NuxtLink>
+          </el-col>
+          <el-col :span="12">
+          </el-col>
+        </el-row>
+      </el-header>
+      <main>
+        <slot />
+      </main>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.el-header {
+  background-color: #ffffff;
+  color: #333;
+  line-height: 60px;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+}
+
+.user {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  font-weight: bold;
+  font-size: 18px;
+}
+</style>
---
+++ src/ui/pages/signout.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const handleSignout = () => {
+  navigateTo('signin')
+};
+
+definePageMeta({
+  layout: "auth"
+});
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">Are you sure you want to sign out?</div>
+    <div class="buttons">
+      <el-button color="#0077FF" @click="handleSignout">Sign out</el-button>
+      <el-button color="#0077FF" @click="() => navigateTo('/')">Back to main</el-button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 20%;
+  margin: auto;
+}
+
+.title {
+  color: black;
+  font-weight: bold;
+  font-size: 18px;
+  padding: 15px;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
